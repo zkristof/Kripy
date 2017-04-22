e22f1710815dbac499fe1d8cd44b06f26714b516
zkristof <zkristof@gmail.com>
2017-04-22 08:12:25 +0200

    php_globals
    How globals works...

---
+++ kripy.php
@@ -0,0 +1,17 @@
+ <!DOCTYPE html>
+<html>
+<body>
+ <?php
+$x = 75;
+$y = 25;
+ 
+function addition() {
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
+ 
+addition();
+echo $z;
+?> 
+
+</body>
+</html> 
