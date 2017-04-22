88a22194e4672d4b2d9c0e70d70871b8281eb2ab
zkristof <zkristof@gmail.com>
2017-04-22 08:24:00 +0200

    Multiplication operator
    https://www.w3schools.com/php/php_operators.asp

--- kripy.php
+++ kripy.php
@@ -2,16 +2,29 @@
 <html>
 <body>
  <?php
-$x = 75;
-$y = 25;
+$x = 4;
+$y = 4;
  
 function addition() {
-    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+    $GLOBALS['z'] = $GLOBALS['x'] * $GLOBALS['y'];
 }
  
 addition();
 echo $z;
 ?> 
+<?php
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+?>
 
 </body>
 </html> 
